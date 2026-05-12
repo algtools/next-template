4ed1eaf015bc8b21cecbe91aaff1a5209e9b1a96
azaelvaldo <azael_valdo@hotmail.com>
2026-05-11 18:43:32 -0600

    Update commitlint.config.cjs

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -3,5 +3,12 @@ module.exports = {
 	extends: ["@commitlint/config-conventional"],
 	rules: {
 		"header-max-length": [2, "always", 200],
+		"body-max-line-length": [0, "always", 100],
+		// Enforce lowercase subject (forbid sentence-case, start-case, pascal-case, upper-case)
+		"subject-case": [
+			0,
+			"never",
+			["sentence-case", "start-case", "pascal-case", "upper-case"],
+		],
 	},
 };
